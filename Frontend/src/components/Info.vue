a28ff01a9761577eece41cdee1938a1bb6710712
jondo <jondo@pop-os.localdomain>
2021-08-05 17:02:35 -0500

    check when someone is typing and styling

--- Frontend/src/components/Info.vue
+++ Frontend/src/components/Info.vue
@@ -1,26 +1,31 @@
 <template>
     <div class="grid grid-rows-3 gap-4">
         <div class="bg-gray-100 rounded-2xl grid">
-            <button @click="$emit('closeInfo')" class="self-start m-4 md:hidden">
+            <button @click="$emit('closeInfo')"
+            class="self-start justify-self-start m-4 md:hidden hover:text-gray-500 transition-colors duration-200">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
                 </svg>
             </button>
-            <button class="self-end m-4 flex">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 self-center text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                </svg>
-                <p class="ml-2 text-red-500 mt-0.5 ">Salir del grupo</p>
-            </button>
+            <div class="justify-self-stretch md:place-self-stretch grid">
+                <img src="../assets/room3.png" class="justify-self-center md:self-end md:mt-8 rounded-full h-28 max-w-xs md:max-h-52 md:max-w-lg">
+                <button
+                class="justify-self-start md:self-end m-4 flex text-red-500 hover:text-red-300 transition-colors duration-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 self-center" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                    </svg>
+                    <p class="ml-2 self-center mt-1">Salir de la sala</p>
+                </button>
+            </div>
         </div>
         <div class="bg-gray-100 row-span-2 rounded-2xl p-4">
-            <p class="text-center md:text-3xl text-gray-500 mb-4">Lista de usuarios activos</p>
+            <p class="text-center md:text-2xl text-gray-500 mb-4">Lista de usuarios activos</p>
             <ul v-for="(user, index) in userList" v-bind:key="`user${index}`">
-                <div class="flex">
+                <div class="flex font-mono">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-700 self-center" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    <li class="ml-4 mt-0.5 md:text-3xl">{{user}}</li>
+                    <li class="ml-4 mt-0.5 md:text-2xl">{{user}}</li>
                 </div>
             </ul>
         </div>
--- Frontend/src/components/Input.vue
+++ Frontend/src/components/Input.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <form v-on:submit.prevent="sendMessage" class="grid grid-cols-12">
-            <button class="grid justify-center place-items-center bg-pink-300 rounded-l-full hover:bg-pink-400 col-span-2 md:col-span-1">
+        <section class="grid grid-cols-12 relative">
+            <Picker v-show="showEmojis" :data="emojiIndex" set="twitter" @select="addEmoji"
+            class="absolute bottom-full"/>
+             <button
+             class="grid justify-center place-items-center bg-pink-300 rounded-l-full hover:bg-pink-400 col-span-2 md:col-span-1"
+             @click="showEmojis = !showEmojis">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
@@ -10,14 +14,16 @@
             type="text"
             placeholder="Escribe tu mensaje"
             v-model="msg"
-            v-on:keyup.enter="sendMessage">
+            @keypress.enter="sendMessage"
+            @focus="showEmojis = false"
+            @input="checkTyping">
             <button class="grid justify-center place-items-center text-m font-extrabold self-center p-5 rounded-r-full bg-pink-300 hover:bg-pink-400 col-span-2 md:col-span-1 pr-7" 
-            type="submit">
+            @click="sendMessage">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
                 </svg>
             </button>
-        </form>
+        </section>
         
     </div>
 </template>
@@ -26,32 +32,67 @@
 import { ref } from "vue"
 import { useStore } from 'vuex'
 
+import data from "emoji-mart-vue-fast/data/all.json";
+// Note: component needs to be imported from /src subfolder:
+import { Picker, EmojiIndex } from "emoji-mart-vue-fast/src";
+import "emoji-mart-vue-fast/css/emoji-mart.css";
+
 export default {
+    components: {
+        Picker,
+    },
+
     setup() {
+        let emojiIndex = new EmojiIndex(data)
         const store = useStore()
         const msg = ref('')
+        var checkMsg = ''
+        const showEmojis = ref(false)
         const socket = store.state.socket
 
 
         socket.on('send message', (msg) => {
             store.commit('receiveMessage', msg)
         })
-        
         socket.on('send active user list', (users) => {
             store.commit('setActiveUsers', users)
         })
         socket.on('user disconnected', (userName) => {
             store.commit('removeActiveUser', userName)
         })
+        socket.on('user typing', (userName) => {
+            store.commit('addUserTyping', userName)
+        })
+        socket.on('user stop typing', (userName) => {
+            store.commit('removeUserTyping', userName)
+        })
 
         const sendMessage = () => {
+            showEmojis.value = false
             if (msg.value){
                 socket.emit('send message', {name: store.state.name, content: msg.value})
                 msg.value = ''
             }
         }
 
-        return{ msg, sendMessage}
+        const addEmoji = (emoji) => {
+            msg.value += emoji.native
+        }
+
+        const checkTyping = () => {
+            socket.emit('user typing')
+            checkMsg = msg.value
+        }
+
+        setInterval(() => {
+            if (checkMsg === '') {
+                socket.emit('user stop typing')
+            } else {
+                checkMsg = ''
+            }
+        }, 2000)
+
+        return{ msg, sendMessage, emojiIndex, showEmojis, addEmoji, checkTyping}
         
     },
 }
